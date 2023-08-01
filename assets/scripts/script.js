6426d1b4f635e0672c529e508a642f98be938e03
rabin-thami <rabinthami36@gmail.com>
2023-08-01 08:49:22 +0545

    Project Completed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,42 @@
-const buttons = document.querySelectorAll('.key-button')
-const phoneMessage = document.querySelector('#phoneMessge')
+const buttons = document.querySelectorAll('.key-button');
+const displayPagerMessage = document.querySelector('#pagerMessage');
+const phoneMessage = document.querySelector('#phoneMessge');
+const submitResetButtons = document.querySelectorAll('.btn');
+const notification = new Audio('assets/sound/pager.wav')
 
-const submitResetBtn = document.querySelector("#btn")
 
-
-buttons.forEach(button => {
-  button.addEventListener('click', function(){
-    const buttonValue = button.innerText
-    phoneMessage.textContent += buttonValue
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function(){
+    const buttonValue = buttons[i].innerText;
+    phoneMessage.textContent += buttonValue;
   });
-});
+}
+
+
+for (let i = 0; i < submitResetButtons.length; i++) {
+  submitResetButtons[i].addEventListener('click', function() {
+    const button = submitResetButtons[i];
+    if (button.id === 'submit') {
+      sentToPage()
+    } else if (button.id === 'reset') {
+      resetPagerAndPhone()
+    }
+  });
+}
+
+
+function sentToPage(){
+  if(!phoneMessage.innerText) {
+    alert("You can't sent empty message")
+  } else {
+    displayPagerMessage.innerText = phoneMessage.innerText
+    phoneMessage.innerText = ''
+    notification.play()
+
+  }
+}
+
+function resetPagerAndPhone() {
+  phoneMessage.innerText = ''
+  displayPagerMessage.innerText = ''
+}
